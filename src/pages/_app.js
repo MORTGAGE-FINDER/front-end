e99f0edabbf8dd7305bbf8eb30f8aec24e98d782
Keyan <keyantabor98@gmail.com>
2023-02-21 14:58:12 -0500

    add ground work

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import React from 'react'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
+import '@/styles/globals.css'
+import Layout from './Layout'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Layout>
+    <Component {...pageProps} />
+      </Layout>
+    </UserProvider >
+  )
 }
---
+++ src/pages/landing.js
@@ -0,0 +1,14 @@
+export default function Landing() {
+    return(
+        <>
+        <div className="container">
+            <h1>Loan Genie</h1>
+            <image href="http://via.placeholder.com/300"></image>
+            <h2>Sign in to get started!</h2>
+            
+            </div>
+        </>
+
+
+    )
+}
