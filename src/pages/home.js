4829c8dbfd9b521fe79a264ccd908fc39c0dd8b1
Keyan Tabor <keyantabor98@gmail.com>
2023-02-21 17:58:08 -0500

    updates to navbar

---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+return(
+<>
+    <h1>Loan Genie</h1>
+    <image href="http://via.placeholder.com/300"></image>
+    <p>Loan genie is a quick and easy way to determine if you MAY be eligible for a mortgage loan. We accomplished this using machine learning based off thousands of applicants from all over the United States. On the next screen we will ask you a few questions to help us get to know you more!</p>
+</>
+)
+}
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,11 +1,13 @@
+import Image from "next/image"
+
 export default function Landing() {
     return(
         <>
         <div className="container">
             <h1>Loan Genie</h1>
-            <image href="http://via.placeholder.com/300"></image>
+            <Image href="http://via.placeholder.com/300" alt='filler' />
             <h2>Sign in to get started!</h2>
-            
+            <a href="/api/auth/login">Sign In</a>
             </div>
         </>
 
