8922fba9bd8761e94ed363d47fe27aec0ea12129
EricMungai97 <mungaikinus@gmail.com>
2023-02-23 01:34:48 -0800

    newchanges

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,9 @@
 import Image from "next/image"
 export default function About() {
   return (
-    <div className="flex-column">
-      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mt-4 w-30">
+    <div className="bg-[url('/images/krakenimages-Y5bvRlcCx8k-unsplash.jpg')] h-screen w-screen bg-contain bg-center">
+    <div className="flex justify-center">
+      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mr-20 mt-4 w-30">
         <div class="flex-column">
           <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U03V0N804HE-a1af50c10b55-192" width="200" height="200" alt="Avatar"/>
             <div>
@@ -13,26 +14,7 @@ export default function About() {
       </a>
       <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mt-4 w-30">
         <div class="flex-column">
-          <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U03V0N804HE-a1af50c10b55-192" width="200" height="200" alt="Avatar"/>
-            <div>
-              <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Full Stack Software Developer</h5>
-              <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </div>
-        </div>
-      </a>
-      <div className="">
-      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mt-4 w-30">
-        <div class="flex-column">
-          <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U03V0N804HE-a1af50c10b55-192" width="200" height="200" alt="Avatar"/>
-            <div>
-              <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Full Stack Software Developer</h5>
-              <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </div>
-        </div>
-      </a>
-      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mt-4 w-30">
-        <div class="flex-column">
-          <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U03V0N804HE-a1af50c10b55-192" width="200" height="200" alt="Avatar"/>
+          <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U043BMUTN8Y-af494fd60f05-512" width="200" height="200" alt="Avatar"/>
             <div>
               <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Full Stack Software Developer</h5>
               <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
@@ -40,7 +22,26 @@ export default function About() {
         </div>
       </a>
       </div>
-      
-    </div>
+      <div className="flex justify-center">
+      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mr-20  mt-4 w-30">
+        <div class="flex-column">
+          <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U03L05MFV0D-c579665bc967-512" width="200" height="200" alt="Avatar"/>
+            <div>
+              <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Full Stack Software Developer</h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </div>
+      </a>
+      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 mt-4 w-30">
+        <div class="flex-column">
+          <Image class="h-30 w-30 rounded-full object-cover mx-auto " src="https://ca.slack-edge.com/T039KG69K-U03UK4DJ5JR-52754228f88f-512" width="200" height="200" alt="Avatar"/>
+            <div>
+              <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Full Stack Software Developer</h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </div>
+      </a>
+      </div>
+      </div>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
 export default function Home() {
     return (
 
-        <>
+        <div className="bg-[url('/images/dillon-kydd-2keCPb73aQY-unsplash.jpg')] h-screen w-screen bg-cover bg-center">
             <h1 className="text-center text-6xl text-[#1EBBD7]">Loan Genie</h1>
             <div className="flex justify-center my-8">
-                <img src="http://via.placeholder.com/300" width="300" height="900" alt="placeholder" />
+                {/* <img src="http://via.placeholder.com/300" width="300" height="900" alt="placeholder" /> */}
             </div>
-            <p className="max-w-md mx-auto text-center text-gray-600">Loan genie is a quick and easy way to determine if you MAY be eligible for a mortgage loan. We accomplished this using machine learning based off thousands of applicants from all over the United States. On the next screen we will ask you a few questions to help us get to know you more!</p>
-        </>
+            <div className="text-center">
+            <p className="max-w-md mt-60 mx-auto  text-white">Loan genie is a quick and easy way to determine if you MAY be eligible for a mortgage loan. We accomplished this using machine learning based off thousands of applicants from all over the United States. On the next screen we will ask you a few questions to help us get to know you more!</p>
+            <Link href="./Services" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-7 ">Get Started</Link>
+            </div>
+            
+        </div>
     )
 }
